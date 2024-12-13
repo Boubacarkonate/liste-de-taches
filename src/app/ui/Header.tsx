92061ddb926bbf22835a221ff4e2223c2085ecd1
Bouba <boubacar.konate@outlook.fr>
2024-12-13 10:58:15 +0100

    changement de titre

--- src/app/ui/Header.tsx
+++ src/app/ui/Header.tsx
@@ -5,7 +5,7 @@ const Header: React.FC = () => {
   return (
     <header className="bg-gradient-to-r from-indigo-600 via-purple-500 to-pink-500 shadow-md text-white py-4">
       <div className="container mx-auto py-4 px-6 flex justify-between items-center">
-        <h1 className="text-xl font-bold">Gestionnaire de Tâches</h1>
+        <h1 className="text-xl font-bold">TaskMaster</h1>
         <nav>
           <ul className="flex space-x-4">
             <li>
